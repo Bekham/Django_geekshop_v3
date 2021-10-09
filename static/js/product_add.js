0c491536d765598737c17f0aa5dfc1a1f213180f
Bekham <anton-bekker@yandex.ru>
2021-10-09 23:52:18 +0600

    Вопрос! Попытался реализовать в Каталоге поочередное отображение кнопок "Удалить и корзины" и "Отправить в корзину" через Ajax. Однако, при использовании фильтрации категорий и добавлении всех товаров в корзину с пагинатором - на второй странице происходит перемешивание позиций товаров и появляются позиции из первой странице. Помогите! Устал голову ломать!

--- static/js/product_add.js
+++ static/js/product_add.js
@@ -1,18 +1,15 @@
 window.onload = function (){
-    $('.product_add').on('click', 'input[type="button"]',function (){
-        let t_href = event.target;
-        console.log(t_href.name);
-        console.log(t_href.value);
+    $('.product_add').on('click', 'input[type="button"]', function () {
+        let t_hrep = event.target;
+        console.log(t_hrep.name);
+        console.log(t_hrep.id);
         $.ajax(
             {
-                url: '/products/product_add/' + t_href.name + "/" + t_href.value + "/",
+                url: '/products/product_add/' + t_hrep.name + "/" + t_hrep.value + "/",
                 success: function (data) {
-                $('.product_add').html(data.result);
-            },
-
+                    $('.product_add').html(data.result);
+                },
             });
         event.preventDefault();
-
-
     });
     }
