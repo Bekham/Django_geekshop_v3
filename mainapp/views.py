8461f79d8f78fac9e68e4a7ab7a8f065e984cb89
Bekham <anton-bekker@yandex.ru>
2021-10-09 23:46:16 +0600

    Организовать фильтрацию продуктов по категориям Организовать постраничный вывод в каталоге.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 import os
-import json
 from django.template.loader import render_to_string
-from mainapp.models import Product
+from mainapp.models import Product, ProductCategory
 from django.contrib.auth.decorators import login_required
-from baskets.views import basket_add, basket_remove
+from baskets.views import basket_add
 from baskets.models import Basket
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -19,41 +19,68 @@ def index(request):
         'title': 'GeekShop',
         'head': 'GeekShop Store',
         'now_date': True,
-        # 'user': User.username,
 
     }
     return render(request, 'mainapp/index.html', context)
 
 
 @login_required
-def products(request):
-    _products = Product.objects.all()
-
+def products(request, category_id=None, page_id=1, product_id=None, wtd=None, current_category=0):
+    print(current_category, page_id, category_id)
+    if category_id is not None:
+        _products = Product.objects.filter(category_id=category_id)
+        if current_category != 0:
+            current_category = current_category
+        else:
+            current_category = category_id
+    elif current_category != 0:
+        _products = Product.objects.filter(category_id=current_category)
+    else:
+        _products = Product.objects.all()
+    print('end', current_category, page_id, category_id)
+    paginator = Paginator(_products, per_page=2)
+    try:
+        products_paginator = paginator.page(page_id)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
     context = {
         'title': 'GeekShop - Каталог',
         'now_date': False,
-        'products': _products,
+        'products': products_paginator,
         'baskets': Basket.objects.filter(user=request.user).values('product_id'),
+        'categories': ProductCategory.objects.all(),
+        'current_category': current_category,
     }
-    print(context['baskets'])
-    return render(request, 'mainapp/products.html', context)
-
-
-@login_required
-def product_add(request, product_id, wtd):
     if request.is_ajax():
-        if wtd == 'Отправить в корзину':
-            basket_add(request, product_id=product_id)
-        elif wtd == 'Удалить из корзины':
-            # basket_add(request, product_id=product_id)
-            basket_remove(request, product_id=product_id)
-        context = {
-            'title': 'GeekShop - Каталог',
-            'products': Product.objects.all(),
-            'baskets': Basket.objects.filter(user=request.user).values('product_id'),
-        }
+        if wtd is not None:
+            if wtd == 'Отправить в корзину':
+                basket_add(request, product_id=product_id)
+            elif wtd == 'Удалить из корзины':
+                Basket.objects.filter(product_id=product_id).filter(user_id=request.user).delete()
+            result = render_to_string('mainapp/goods.html', context)
+            return JsonResponse({'result': result})
+        # if category_id is not None:
+        #     result = render_to_string('mainapp/category.html', context)
+        #     return JsonResponse({'result': result})
+    else:
+        return render(request, 'mainapp/products.html', context)
 
-        result = render_to_string('mainapp/goods.html', context)
-        return JsonResponse({'result': result})
+
+# @login_required
+# def product_add(request, product_id, wtd):
+#     if request.is_ajax():
+#         if wtd == 'Отправить в корзину':
+#             basket_add(request, product_id=product_id)
+#         elif wtd == 'Удалить из корзины':
+#             Basket.objects.filter(product_id=product_id).filter(user_id=request.user).delete()
+#         context = {
+#             'title': 'GeekShop - Каталог',
+#             'products': Product.objects.all(),
+#             'baskets': Basket.objects.filter(user=request.user).values('product_id'),
+#         }
+#         result = render_to_string('mainapp/goods.html', context)
+#         return JsonResponse({'result': result})
 
 
