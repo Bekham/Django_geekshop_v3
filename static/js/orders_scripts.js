2b39456fb0fdf5b0689e30eebc538a8bd45053d1
Bekham <anton-bekker@yandex.ru>
2021-10-26 21:28:07 +0600

    Реализовать обновление статистики заказа через jQuery.  Расширить функционал работы с формами при помощи «django-dynamic-formset».  Реализовать асинхронное обновление цены при добавлении нового продукта в заказ.

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -1,4 +1,37 @@
 window.onload = function () {
+    $('.product_add').on('click', 'input[type="button"]', (e) => {
+            let t_href = e.target;
+            let csrf_token = $('meta[name="csrf-token"]').attr('content');
+            console.log(t_href.id)
+
+
+            $.ajax(
+            {
+                type:'POST',
+
+                headers: {"X-CSRFToken": csrf_token},
+                url: '/baskets/add/' + t_href.name + "/" ,
+                dataType: 'json',
+                data: {
+                    page_id: t_href.id,
+
+                //     // csrfmiddlewaretoken: $("input[name='csrfmiddlewaretoken']").val()},
+                //     csrfmiddlewaretoken: '{{ csrf_token }}'
+                },
+                // cache: false,
+                success:  (data) => {
+                    if (data) {
+                        console.log(data);
+                        $('.product_add').html(data.result);
+                    }
+
+
+            },
+
+            });
+            e.preventDefault();
+        });
+
 
     let _quantity, _price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
 
@@ -66,7 +99,7 @@ window.onload = function () {
 
     function  deleteOrderItem(row) {
         let target_name = row[0].querySelector('input[type="number"]').name;
-        // quantity_arr[orderitem_num] = 0;
+        quantity_arr[orderitem_num] = 0;
         orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
         delta_quantity = -quantity_arr[orderitem_num];
         orderSummerUpdate(price_arr[orderitem_num], delta_quantity)
@@ -85,23 +118,36 @@ window.onload = function () {
 
 
     }
-
+    if (!order_total_quantity) {
+        orderSummaryRecalc();
+}
     function orderSummaryRecalc() {
        order_total_quantity = 0;
        order_total_cost = 0;
-
+       total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val())
        for (var i=0; i < total_forms; i++) {
+            _quantity = parseInt($('input[name=orderitems-' + i + '-quantity').val());
+            _price = parseInt($('.orderitems-' + i + '-price').text().replace(',', '.'));
+           quantity_arr[i] = _quantity
+        if (_price) {
+            price_arr[i] = _price;
+
+        } else {
+            price_arr[i] = 0
+        }
            order_total_quantity += quantity_arr[i];
-           order_total_price += quantity_arr[i] * price_arr[i];
+           order_total_cost += quantity_arr[i] * price_arr[i];
+           console.log(price_arr[i])
        }
        $('.order_total_quantity').html(order_total_quantity.toString());
-       $('.order_total_cost').html(order_total_price.toString() + ',00');
-       // $('.order_total_cost').html(Number(order_total_cost.toFixed(2)).toString());
+       $('.order_total_cost').html(Number(order_total_cost.toFixed(2)).toString());
     }
+
 
 
     $('.order_form select').change(function () {
         var target = event.target;
+        console.log(target)
         orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
         var orderitem_product_pk = target.options[target.selectedIndex].value;
         console.log(orderitem_product_pk)
@@ -118,6 +164,7 @@ window.onload = function () {
                    let price_html = '<span>' + data.price.toString().replace('.', ',') +'</span> руб';
                    let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
                    current_tr.find('td:eq(2)').html(price_html);
+                   console.log(data.price)
 
                    if (isNaN(current_tr.find('input[type="number"]').val())) {
                        current_tr.find('input[type="number"]').val(0);
